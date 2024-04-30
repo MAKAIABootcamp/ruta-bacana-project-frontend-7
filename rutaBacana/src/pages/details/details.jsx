d15787951b7f70f8cff29ddd65c799692c787b78
cristian ojito <criztianvelez@gmail.com>
2024-04-29 20:09:50 -0500

    header y detalles

--- rutaBacana/src/pages/details/details.jsx
+++ rutaBacana/src/pages/details/details.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import Header from '../../componentes/Header/Header'
+import Details from '../../componentes/Details/Details'
+import FotoRaquira from '../../componentes/FotoRaquira/FotoRaquira'
 const details = () => {
   return (
-    <div>details</div>
+    <div>
+      <div className='letras'>
+      <Header />
+      <FotoRaquira />
+      <Details />
+      </div>
+    </div>
   )
 }
 export default details
