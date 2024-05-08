7c6fa85d2d6dc7ab98bf1bcc5294b8b149f48e52
Valentina Florez <valenflorca@gmail.com>
2024-05-08 08:08:02 -0500

    carousel and firebase

--- rutaBacana/src/pages/home/home.jsx
+++ rutaBacana/src/pages/home/home.jsx
@@ -3,12 +3,16 @@ import ImagenHome from "../../componentes/ImagenHome/ImagenHome";
 import Filtros from "../../componentes/Filtros/Filtros";
 import "./home.scss";
 import Header from "../../componentes/Header/Header";
+import Carrusel from "../../componentes/Carrusel/Carrusel";
 
 const home = () => {
+ 
+
   return (
     <div>
       <div className="containerHome">
         <Header />
+        <Carrusel />
         <Filtros />
         <ImagenHome />
       </div>
