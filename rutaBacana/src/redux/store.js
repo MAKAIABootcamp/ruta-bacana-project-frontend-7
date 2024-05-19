b1f1293a57adb209846e66d98bfd81b0637b5dc3
EmmanuelUs4 <emmanueljusme24@gmail.com>
2024-05-18 19:36:41 -0500

    adding google maps no api key yet

--- rutaBacana/src/redux/store.js
+++ rutaBacana/src/redux/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import favoritosReducer from "./favoritos/favoritosSlice";
-import userAuthReducer from ".//userAuth/userAuthSlice"
+import userAuthReducer from "./userAuth/userAuthSlice";
 import destinosReducer from "./Destinos/destinosSlice";
 import comentariosReducer from "./comentarios/comentariosSlice";
-import negociosReducer from './negocios/negociosSlice'
+import negociosReducer from './negocios/negociosSlice';
 import scrollReducer from './scroll/scrollSlice';
+import locationReducer from './location/locationSlice';
 
 const store = configureStore({
   reducer: {
@@ -13,7 +14,8 @@ const store = configureStore({
     comentarios: comentariosReducer,
     favoritos: favoritosReducer,
     negocios: negociosReducer,
-    scroll: scrollReducer
+    scroll: scrollReducer,
+    location: locationReducer
   },
   devTools: process.env.NODE_ENV !== "production",
 });
