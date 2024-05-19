c06cec64799d8d62a88d666ca74654ed8ee65759
Lina Naranjo <lina.naja1807@gmail.com>
2024-05-18 19:46:46 -0500

    Botón de agregar destinos

--- rutaBacana/src/pages/home/home.jsx
+++ rutaBacana/src/pages/home/home.jsx
@@ -1,19 +1,33 @@
 import React from "react";
 import Filtros from "../../componentes/Filtros/Filtros";
 import "./home.scss";
-import Footer from "../../componentes/Footer/Footer"
+import Footer from "../../componentes/Footer/Footer";
 import ListDestinos from "../../componentes/ListDestinos/ListDestinos";
 import Carrusel from "../../componentes/Carrusel/Carrusel";
+import { HiViewGridAdd } from "react-icons/hi";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-const home = () => { 
-
+const home = () => {
+  const navigate = useNavigate();
+  const { user } = useSelector((store) => store.userAuth);
+  const isAdminEmail = user && user.email === "rutabacana@gmail.com";
   return (
     <div>
       <div className="containerHome">
         <Carrusel />
+        <div className="agregarDestino">
+          {isAdminEmail && (
+            <HiViewGridAdd
+              onClick={() => navigate(`/agregarDestinos/`)}
+              size={40}
+              style={{ cursor: "pointer" }}
+            />
+          )}
+        </div>
         <div className="destinosContainer">
           <Filtros />
-          <ListDestinos/>
+          <ListDestinos />
         </div>
         <Footer />
       </div>
