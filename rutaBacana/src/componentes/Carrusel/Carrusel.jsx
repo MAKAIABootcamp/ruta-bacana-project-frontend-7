ccde91a4ac2633160efd96d53b7d90543b53003f
EmmanuelUs4 <emmanueljusme24@gmail.com>
2024-05-19 15:24:39 -0500

    fixing minor style config

--- rutaBacana/src/componentes/Carrusel/Carrusel.jsx
+++ rutaBacana/src/componentes/Carrusel/Carrusel.jsx
@@ -22,8 +22,8 @@ const Carrusel = () => {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
-    // autoplay: true,
-    // autoplaySpeed: 1500
+    autoplay: true,
+    autoplaySpeed: 1500
   };
 
   return (
